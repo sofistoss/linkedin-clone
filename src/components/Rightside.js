a8af7974e55bdeeb51886117a7e782d5f8b60a56
sofistoss <sofiano.a@hotmail.fr>
2021-08-19 03:25:51 +0100

    leftside

---
+++ src/components/Rightside.js
@@ -0,0 +1,11 @@
+import React from "react";
+import styled from "styled-components";
+
+const Rightside = () => {
+  return <Container>right side</Container>;
+};
+
+const Container = styled.div`
+  grid-area: rightside;
+`;
+export default Rightside;
