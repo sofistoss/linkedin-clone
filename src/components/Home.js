f4421b7ca2e850cebf23e82fdd77e88ec16ba914
sofistoss <sofiano.a@hotmail.fr>
2021-08-20 03:33:56 +0100

    2nd leftside

--- src/components/Home.js
+++ src/components/Home.js
@@ -71,7 +71,6 @@ const Layout = styled.div`
   grid-template-columns: minmax(0, 5fr) minmax(0, 12fr) minmax(300px, 7fr);
   column-gap: 25px;
   row-gap: 25px;
-  /* grid-template-row: auto; */
   grid-template-rows: auto;
   margin: 25px 0;
   @media (max-width: 768px) {
--- src/components/Leftside.js
+++ src/components/Leftside.js
@@ -15,6 +15,21 @@ const Leftside = () => {
             <AddPhotoText>Add a photo</AddPhotoText>
           </a>
         </UserInfo>
+        <Widget>
+          <a>
+            <div>
+              <span>Connections</span>
+              <span>Grow your network</span>
+            </div>
+            <img src="/images/widget-icon.svg" alt="" />
+          </a>
+        </Widget>
+        <Item>
+          <span>
+            <img src="/images/item-icon.svg" alt="" />
+            My Items
+          </span>
+        </Item>
       </ArtCard>
     </Container>
   );
@@ -43,6 +58,7 @@ const UserInfo = styled.div`
 
 const CardBackground = styled.div`
   background-image: url(/images/card-bg.svg);
+  background-repeat: no-repeat;
   background-position: center;
   background-size: 462px;
   height: 54px;
@@ -65,8 +81,78 @@ const Photo = styled.div`
   border-radius: 50%;
 `;
 
-const Link = styled.div``;
+const Link = styled.div`
+  font-size: 16px;
+  line-height: 1.5;
+  color: rgba(0, 0, 0, 0.9);
+  font-weight: 600;
+`;
 
-const AddPhotoText = styled.div``;
+const AddPhotoText = styled.div`
+  color: #0a66c2;
+  margin-top: 4px;
+  font-size: 12px;
+  line-height: 1.33;
+  font-weight: 400;
+`;
+
+const Widget = styled.div`
+  border-bottom: 1px solid rgba(0, 0, 0, 0.15);
+  padding-top: 12px;
+  padding-bottom: 12px;
+
+  & > a {
+    text-decoration: none;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 4px 12px;
+
+    &:hover {
+      background-color: rgba(0, 0, 0, 0.08);
+    }
+
+    div {
+      display: flex;
+      flex-direction: column;
+      text-align: left;
+      span {
+        font-size: 12px;
+        line-height: 1.333;
+        &:first-child {
+          color: rgba(0, 0, 0, 0.6);
+        }
+        &:nth-child(2) {
+          color: rgba(0, 0, 0, 1);
+        }
+      }
+    }
+  }
+
+  svg {
+    color: rgba(0, 0, 0, 1);
+  }
+`;
+
+const Item = styled.a`
+  border-color: rgba(0, 0, 0, 0.8);
+  text-align: left;
+  padding: 12px;
+  font-size: 12px;
+  display: block;
+
+  span {
+    display: flex;
+    align-items: center;
+    color: rgba(0, 0, 0, 1);
+    svg {
+      color: rgba(0, 0, 0, 0.6);
+    }
+  }
+
+  &:hover {
+    background-color: rgba(0, 0, 0, 0.08);
+  }
+`;
 
 export default Leftside;
